b4e627b4ab9a6b24611d0d1ac364f542aee6be52
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-05-26 12:05:22 -0400

    Add functionality to append a train input to the top of the page. Input database snapshot functionality. Entered Moment.js to manipulate time but did not get the correct time to show so far.

--- assets/JavaScript/TrainScheduler-.js
+++ assets/JavaScript/TrainScheduler-.js
@@ -12,15 +12,90 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-var trainName = "";
-var destination = "";
-var firstTrainTime = "";
-var frequnecy = "";
+// var trainName = "";
+// var destination = "";
+// var firstTrainTime = parseInt("");
+// var frequency = "";
+
+
+// below converts to military time
+// var time = moment("02:30 PM", "hh:mm A").format("HH:hh")
+// console.log(time);
+
 
 $("#tSubmit").on("click", function (event) {
     event.preventDefault();
     // Grabbed values from text-boxes
-    trainName = $("train-name").val().trim();
-    destination = $("#destination").val().trim();
-    firstTrainTime = $("#first-train").val().trim();
-    frequnecy = $("#frequency").val().trim();
+    var trainName = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrainTime = $("#first-train").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    var newTrain = {
+        dbtrainName: trainName,
+        dbdestination: destination,
+        dbfirstTrainTime: firstTrainTime,
+        dbfrequency: frequency,
+
+    };
+    //upload new train data to database
+    database.ref().push(newTrain);
+    // log to console
+    console.log(newTrain.dbtrainName);
+    console.log(newTrain.dbdestination);
+    console.log(newTrain.dbfirstTrainTime);
+    console.log(newTrain.dbfrequency);
+
+
+    // clear text boxes
+
+
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train").val("");
+    $("#frequency").val("");
+
+});
+
+//create Firebase event for adding train to database and row in chart in html when input is added
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val())
+
+    //store snapshots in variables
+
+    var trainName = childSnapshot.val().dbtrainName;
+    var destination = childSnapshot.val().dbdestination;
+    var firstTrainTime = childSnapshot.val().dbfirstTrainTime;
+    var frequency = childSnapshot.val().dbfrequency;
+
+    // train data
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTrainTime);
+    console.log(frequency);
+
+    //calculate next arrival
+    var nextArrival = moment(firstTrainTime, "HH:mm").from(moment(frequency, "m"));
+    nextArrival = moment().format("HH:mm");
+    console.log("Next arrival: " + nextArrival);
+    //calculate how many mins until next train
+    
+    var minsArrive = nextArrival - frequency;
+    console.log("Minutes until next train: " + minsArrive);
+    //create the new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(firstTrainTime),
+        $("<td>").text(frequency),
+        $("<td>").text(nextArrival),
+        $("<td>").text(minsArrive),
+    );
+      //putting new row into table
+      $("#train-table").append(newRow);
+
+    //   function to run an error message
+    }, function(errorObject) {
+        console.log("Errors handled:" + errorObject.code);
+
+});
